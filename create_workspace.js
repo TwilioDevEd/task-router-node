8a429c53b590c52c8556b8a2e42024980e21c113
Victor Shyba <victor.shyba@gmail.com>
2016-06-28 19:45:34 -0300

    create task queues

--- create_workspace.js
+++ create_workspace.js
@@ -28,10 +28,11 @@ if(!hostValue || !phoneNumbers){
 }
 
 function exitErrorHandler(error) {
-  console.error('Error while creating workspace:');
+  console.error('An error occurred:');
   console.error(error);
   process.exit(1);
 }
+
 var workspaceJson = JSON.parse(fs.readFileSync('workspace.json', 'utf8'));
 
 workspaceHelper.deleteByName(workspaceJson.name).then(function(){
@@ -39,13 +40,31 @@ workspaceHelper.deleteByName(workspaceJson.name).then(function(){
   return workspaceHelper.create(workspaceJson.name, eventCallback).then(function(workspace){
     console.log('Workspace "%s" created, eventCallback is %s', workspaceJson.name, eventCallback);
 
-    workspaceJson.workers.forEach(function (workerJson) {
-      var workerAttributes = util.format(JSON.stringify(workerJson.attributes), phoneNumbers[workerJson.name]);
-      console.log('Creating worker %s with attributes %s', workerJson.name, workerAttributes);
-      workspace.workers.create({
-        friendlyName: workerJson.name,
-        attributes: workerAttributes
-      }).catch(exitErrorHandler);
+    workspace.activities.list().then(function(response){
+      var idleActivity = response.activities.find(function(activity){return activity.friendly_name === 'Idle';});
+      var busyActivity = response.activities.find(function(activity){return activity.friendly_name === 'Busy';});
+      var reservedActivity = response.activities.find(function(activity){return activity.friendly_name === 'Reserved';});
+
+      workspaceJson.workers.forEach(function (workerJson) {
+        var workerAttributes = util.format(JSON.stringify(workerJson.attributes), phoneNumbers[workerJson.name]);
+        console.log('Creating worker %s with attributes %s', workerJson.name, workerAttributes);
+        workspace.workers.create({
+          friendlyName: workerJson.name,
+          attributes: workerAttributes,
+          activitySid: idleActivity.sid
+        }).catch(exitErrorHandler);
+      });
+
+      workspaceJson.task_queues.forEach(function(taskQueueJson){
+        workspace.taskQueues.create({
+          friendlyName: taskQueueJson.name,
+          targetWorkers: taskQueueJson.targetWorkers,
+          assignmentActivitySid: busyActivity.sid,
+          reservationActivitySid: reservedActivity.sid
+        }).then(function(response){
+          console.log('"%s" Task Queue created, targeting "%s".', response.friendly_name, response.target_workers);
+        }).catch(exitErrorHandler);
+      });
     });
   }).catch(exitErrorHandler);
 }).catch(exitErrorHandler);
