3815f3027af708eab02b3778b0d3a09efe4dc920
Victor Shyba <victor.shyba@gmail.com>
2016-06-30 11:48:36 -0300

    handle task.canceled

--- routes/events.js
+++ routes/events.js
@@ -1,11 +1,28 @@
 'use strict';
 
 var express = require('express'),
+  MissedCall = require('../models/call'),
   router = express.Router();
 
 // POST /events
 router.post('/', function (req, res) {
-  res.send('ok');
+  var eventType = req.body.EventType;
+
+  function saveMissedCall(){
+    var taskAttributes = req.body.TaskAttributes;
+    MissedCall.create({
+      selectedProduct: taskAttributes.selected_product,
+      phoneNumber: taskAttributes.from
+    }, function(err, missedCall) {
+      res.json(missedCall);
+    });
+  }
+
+  var eventHandler = {};
+  eventHandler['task.canceled'] = saveMissedCall;
+  eventHandler['default'] = function() { res.json({}); };
+
+  (eventHandler[eventType] || eventHandler['default'])();
 });
 
 module.exports = router;
--- spec/routes/events.spec.js
+++ spec/routes/events.spec.js
@@ -18,15 +18,26 @@ describe('Record a MissedCall according to event type', function() {
   });
 
   beforeEach(function (done) {
+    this.taskAttributes = {from: '+555', selected_product: 'Rockets'};
      MissedCall.remove({}, done);
   });
 
   describe('TaskRouter event webhook POSTs to /events', function() {
     it('will ignore unwanted events', function(done) {
       var testApp = supertest(app);
-      testApp.post('/events').send({EventType: 'unwanted.event'}).end(function () {
+      testApp.post('/events').send({EventType: 'unwanted.event', TaskAttributes: this.taskAttributes}).end(function () {
         MissedCall.count({}, function(err, docsCount) {
           expect(docsCount).to.be.equals(0);
+          done();
+        });
+      });
+    });
+
+    it('will store a missed call for the case when caller hangs up while waiting', function(done) {
+      var testApp = supertest(app);
+      testApp.post('/events').send({EventType: 'task.canceled', TaskAttributes: this.taskAttributes}).end(function () {
+        MissedCall.count({}, function(err, docsCount) {
+          expect(docsCount).to.be.equals(1);
           done();
         });
       });
