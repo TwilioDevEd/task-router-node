bb160dd88fce0f63c52254ff5de43c616efacdd9
Victor Shyba <victor.shyba@gmail.com>
2016-07-01 18:18:38 -0300

    Adds twiml generator for status change messages

---
+++ spec/lib/twiml-generator.spec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var expect = require('chai').expect,
+  cheerio = require('cheerio'),
+  twimlGenerator = require('../../lib/twiml-generator');
+
+describe('twiml-generator', function() {
+  it('#generateConfirmMessage', function() {
+    var response = twimlGenerator.generateConfirmMessage('New Status'),
+        $ = cheerio.load(response);
+    expect($('say').text()).to.equal('Your status has changed to New Status');
+  });
+});
+
