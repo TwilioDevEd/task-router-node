295030f4a2aa2853e5209e2f7114e5c0ef07b99d
Victor Shyba <victor.shyba@gmail.com>
2016-07-12 21:18:01 -0300

    gets rid of workspace.json

--- create_workspace.js
+++ create_workspace.js
@@ -1,33 +1,10 @@
 #!/usr/bin/env node
 "use strict";
 
-var program   = require('commander'),
-    Q = require('q'),
+var Q = require('q'),
     workspaceHelper = require('./lib/workspace'),
-    util = require('util'),
-    wb = require('twilio/lib/resources/task_router/WorkflowBuilder'),
-    fs = require('fs');
+    wb = require('twilio/lib/resources/task_router/WorkflowBuilder');
 
-var hostValue, phoneNumbers;
-
-program
-  .version('0.0.1')
-  .arguments('<host> <bob_number> <alice_number>')
-  .description('Creates and configures a Twilio\'s TaskRouter Workspace.')
-  .action(function (host, bobNumber, aliceNumber){
-    hostValue        = host;
-    phoneNumbers = {
-      Bob: bobNumber,
-      Alice: aliceNumber
-    };
-  });
-
-program.parse(process.argv);
-
-if(!hostValue || !phoneNumbers){
-  program.outputHelp();
-  process.exit(1);
-}
 
 function exitErrorHandler(error) {
   console.error('An error occurred:');
@@ -35,55 +12,85 @@ function exitErrorHandler(error) {
   process.exit(1);
 }
 
-var workspaceJson = JSON.parse(fs.readFileSync('workspace.json', 'utf8'));
+var workspaceName = 'TaskRouter Node Workspace',
+    host = process.env.HOST,
+    eventCallback = host + '/events';
 
-workspaceHelper.deleteByName(workspaceJson.name).then(function(){
-  var eventCallback = util.format(workspaceJson.event_callback, hostValue);
-  return workspaceHelper.create(workspaceJson.name, eventCallback).then(function(workspace){
-    console.log('Workspace "%s" created, eventCallback is %s', workspaceJson.name, eventCallback);
+workspaceHelper.deleteByName(workspaceName).then(function(){
+  return workspaceHelper.create(workspaceName, eventCallback).then(function(workspace){
+    console.log('Workspace "%s" created, eventCallback is %s', workspaceName, eventCallback);
 
     workspace.activities.list().then(function(response){
       var idleActivity = response.activities.find(byName('Idle'));
       var busyActivity = response.activities.find(byName('Busy'));
       var reservedActivity = response.activities.find(byName('Reserved'));
 
-      workspaceJson.workers.forEach(function (workerJson) {
-        var workerAttributes = util.format(JSON.stringify(workerJson.attributes), phoneNumbers[workerJson.name]);
-        console.log('Creating worker %s with attributes %s', workerJson.name, workerAttributes);
-        workspace.workers.create({
-          friendlyName: workerJson.name,
-          attributes: workerAttributes,
-          activitySid: idleActivity.sid
-        }).catch(exitErrorHandler);
-      });
+      workspace.workers.create({
+        friendlyName: 'Bob',
+        attributes: JSON.stringify({
+            "products": [
+          "ProgrammableSMS"
+          ],
+        "contact_uri": process.env.BOB_NUMBER
+          }),
+        activitySid: idleActivity.sid
+      }).catch(exitErrorHandler);
+      workspace.workers.create({
+        friendlyName: 'Alice',
+        attributes: JSON.stringify({
+            "products": [
+          "ProgrammableVoice"
+          ],
+        "contact_uri": process.env.ALICE_NUMBER
+          }),
+        activitySid: idleActivity.sid
+      }).catch(exitErrorHandler);
 
-      Q.all(workspaceJson.task_queues.map(function(taskQueueJson){
-        return workspace.taskQueues.create({
-          friendlyName: taskQueueJson.name,
-          targetWorkers: taskQueueJson.targetWorkers,
-          assignmentActivitySid: busyActivity.sid,
-          reservationActivitySid: reservedActivity.sid
-        }).then(function(taskQueue){
-          console.log('"%s" Task Queue created, using expression "%s".', taskQueue.friendly_name, taskQueue.target_workers);
-          return taskQueue;
-        });
-      })).then(function(createdQueues){
+      Q.all([workspace.taskQueues.create({
+        friendlyName: 'SMS',
+        targetWorkers: 'products HAS "ProgrammableSMS"',
+        assignmentActivitySid: busyActivity.sid,
+        reservationActivitySid: reservedActivity.sid
+      }), workspace.taskQueues.create({
+        friendlyName: 'Voice',
+        targetWorkers: 'products HAS "ProgrammableVoice"',
+        assignmentActivitySid: busyActivity.sid,
+        reservationActivitySid: reservedActivity.sid
+      }), workspace.taskQueues.create({
+        friendlyName: 'Default',
+        targetWorkers: '1==1',
+        assignmentActivitySid: busyActivity.sid,
+        reservationActivitySid: reservedActivity.sid
+      })]).then(function (createdQueues) {
         var defaultTarget = new wb.WorkflowRuleTarget({
           queue: createdQueues.find(byName('Default')).sid,
-          timeout: 30
+            timeout: 30,
+            priority: 1
         });
 
-        var rules = workspaceJson.workflow.routingConfiguration.map(function(configJson){
-          var target = new wb.WorkflowRuleTarget({
-            queue: createdQueues.find(byName(configJson.targetTaskQueue)).sid,
-            timeout: 30
-          });
-          return new wb.WorkflowRule({
-            expression: configJson.expression,
-            targets: [target, defaultTarget],
-            timeout: 30
-          });
+        var smsTarget = new wb.WorkflowRuleTarget({
+          queue: createdQueues.find(byName('SMS')).sid,
+            timeout: 30,
+            priority: 5
         });
+        var voiceTarget = new wb.WorkflowRuleTarget({
+          queue: createdQueues.find(byName('Voice')).sid,
+            timeout: 30,
+            priority: 5
+        });
+
+        var rules = [
+          new wb.WorkflowRule({
+            expression: 'selected_product=="ProgrammableSMS"',
+            targets: [smsTarget, defaultTarget],
+            timeout: 30
+          }),
+          new wb.WorkflowRule({
+            expression: 'selected_product=="ProgrammableVoice"',
+            targets: [voiceTarget, defaultTarget],
+            timeout: 30
+          }),
+        ];
 
         var taskRouting = new wb.TaskRoutingConfiguration({
           filters : rules,
@@ -94,10 +101,10 @@ workspaceHelper.deleteByName(workspaceJson.name).then(function(){
         }).toJSON();
 
         workspace.workflows.create({
-          friendlyName: workspaceJson.workflow.name,
-          assignmentCallbackUrl: util.format(workspaceJson.workflow.callback, hostValue),
-          fallbackAssignmentCallbackUrl: util.format(workspaceJson.workflow.callback, hostValue),
-          taskReservationTimeout: workspaceJson.workflow.timeout,
+          friendlyName: 'Sales',
+          assignmentCallbackUrl: host + '/call/assignment',
+          fallbackAssignmentCallbackUrl: host + '/call/assignment',
+          taskReservationTimeout: 15,
           configuration: workflowConfiguration
         }).then(function(workflow){
           console.log('Worflow "%s" created.', workflow.friendly_name);
